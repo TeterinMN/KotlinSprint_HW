67a5a9a03e514402861b3adb8e4e84cba3553528
TeterinMN <Scorpion.TMN@ya.ru>
2023-03-28 11:44:09 +0700

    KS-1-2. Создание переменной с работниками и вывод ее в консоль

--- src/main/kotlin/lesson_1/Task_2.kt
+++ src/main/kotlin/lesson_1/Task_2.kt
@@ -3,6 +3,8 @@ package lesson_1
 fun main (){
     var orderNumber = 75
     val gratitudeText = "Спасибо за покупки в нашем интернет магазине!"
-
     println("Заказ №$orderNumber, $gratitudeText")
+
+    var workers = 2000
+    println("В нашем интернет магазине работает $workers работников!")
 }
