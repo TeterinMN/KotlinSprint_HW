3ba4fc1f9924d06433776ad20c03d03a16792d9e
TeterinMN <Scorpion.TMN@ya.ru>
2023-03-28 11:39:21 +0700

    KS-1-2. Вывод в консоль значения всех объявленных ранее переменных

---
+++ src/main/kotlin/lesson_1/Task_2.kt
@@ -0,0 +1,8 @@
+package lesson_1
+
+fun main (){
+    var orderNumber = 75
+    val gratitudeText = "Спасибо за покупки в нашем интернет магазине!"
+
+    println("Заказ №$orderNumber, $gratitudeText")
+}
