706dfd57efddc8cdf000b015dc51a2b4a795c685
TeterinMN <Scorpion.TMN@ya.ru>
2023-03-29 17:14:09 +0700

    KS-2-3. Добавил константу, отредактировал форматирование кода

--- src/main/kotlin/lesson_2/Task_3.kt
+++ src/main/kotlin/lesson_2/Task_3.kt
@@ -1,13 +1,13 @@
 package lesson_2
 
-fun main(){
+const val HOUR = 60
+fun main() {
     val hours = 9
     val minutes = 39
     val travelTime = 457
-    val travelTimeHours = travelTime / 60
-    val travelTimeMinutes = travelTime % 60
-    val resultMinutes = (minutes + travelTimeMinutes) % 60
-    val resultHours = (hours + travelTimeHours) + (minutes + travelTimeMinutes) / 60
-
-            println("$resultHours:$resultMinutes")
+    val travelTimeHours = travelTime / HOUR
+    val travelTimeMinutes = travelTime % HOUR
+    val resultMinutes = (minutes + travelTimeMinutes) % HOUR
+    val resultHours = (hours + travelTimeHours) + (minutes + travelTimeMinutes) / HOUR
+    println("$resultHours:$resultMinutes")
 }
