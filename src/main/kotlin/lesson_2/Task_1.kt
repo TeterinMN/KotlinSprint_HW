a80cf9115e289d04c2ab349846179c78f661e073
TeterinMN <Scorpion.TMN@ya.ru>
2023-03-29 09:20:25 +0700

    KS-2-1. Вычисление средего арифитичеккого

---
+++ src/main/kotlin/lesson_2/Task_1.kt
@@ -0,0 +1,11 @@
+package lesson_2
+
+fun main (){
+    val five = 5
+    val four = 4
+    val three = 3
+    val score = 4.0
+    val result: Double = (three + four + three + five)/score
+
+    println(result)
+}
