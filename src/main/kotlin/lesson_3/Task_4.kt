83d21786543e203d3a3e1cb2f0237bc874dbf49a
TeterinMN <Scorpion.TMN@ya.ru>
2023-03-31 19:36:11 +0900

    KS-1-3. Удалил лишние пробелы

--- src/main/kotlin/lesson_3/Task_4.kt
+++ src/main/kotlin/lesson_3/Task_4.kt
@@ -1,15 +1,15 @@
 package lesson_3
 
 fun main() {
-    var count = "1"
+    var count = 1
     var fromWhere = "E2"
     var toWhere = "E4"
-    val resultOne = "$fromWhere-$toWhere; $count"
+    val resultOne = "$fromWhere-$toWhere;$count"
 
-    count = "2"
+    count = 2
     fromWhere = "D2"
     toWhere = "D3"
-    val resultTwo = "$fromWhere-$toWhere; $count"
+    val resultTwo = "$fromWhere-$toWhere;$count"
 
     println("$resultOne\n$resultTwo")
 }
