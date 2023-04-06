2324f62821c62fd183ac3972fcc00aebb743aa42
TeterinMN <Scorpion.TMN@ya.ru>
2023-04-06 18:59:48 +0900

    KS-4-4. Внес исправления в код

--- src/main/kotlin/lesson_4/Task_4.kt
+++ src/main/kotlin/lesson_4/Task_4.kt
@@ -1,11 +1,11 @@
 package lesson_4
 
 fun main () {
-    val day = 5
+    val day = 4
     val hands = day % 2 == 1
-    val legs = day % 4 != 1
-    val spine = day % 6 != 1
-    val press = day % 8 == 1
+    val legs = day % 2 == 0
+    val spine = day % 2 == 0
+    val press = day % 2 == 1
 
     println("""
         Упражнения для рук:    $hands
