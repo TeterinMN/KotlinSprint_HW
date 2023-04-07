447b78382d77a473abd4a81b9408141487e12b8d
TeterinMN <ScorpionTMN@ya.ru>
2023-04-07 11:37:37 +0900

    KS-4-5. Корректировка кода

--- src/main/kotlin/lesson_4/Task_5.kt
+++ src/main/kotlin/lesson_4/Task_5.kt
@@ -1,4 +1,7 @@
 package lesson_4
+const val MIN_TEAMS = 55
+const val MAX_TEAMS = 70
+const val MIN_PROVISIONS = 50
 
 fun main() {
     print("Есть повреждения коробля? Введите True или False ")
@@ -10,8 +13,8 @@ fun main() {
     print("Погода благоприятная? Введите True или False ")
     val weather = readln().toBoolean()
 
-    if ((!hullDamage && teams >=55 && teams < 70 && provisions > 50 && weather) ||
-        (teams == 70 && provisions > 50 && weather)
+    if ((!hullDamage && teams >= MIN_TEAMS && teams < MAX_TEAMS && provisions > MIN_PROVISIONS && weather) ||
+        (teams == MAX_TEAMS && provisions > MIN_PROVISIONS && weather)
     ) {
         println("Корабль может отправляться")
     } else {
