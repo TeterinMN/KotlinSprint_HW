4880eec68ce75870d5da03e56ffbf4da11722afb
TeterinMN <Scorpion.TMN@ya.ru>
2023-04-09 06:43:03 +0900

    KS-5-4. Задача 4 к Уроку 5 - убрал константы

--- src/main/kotlin/lesson_5/Task_4.kt
+++ src/main/kotlin/lesson_5/Task_4.kt
@@ -1,14 +1,12 @@
 package lesson_5
 
-const val LOGIN = "batman"
-const val PWD = "password"
 fun main() {
     print("Добро пожаловать в приложение. \nВведите логин: ")
     val login = readln()
-    if (login == LOGIN) {
+    if (login == "batman") {
         print("Введите пароль: ")
-        val pwd = readln()
-        if (pwd == PWD) println("Доступ предоставлен!")
+        val password = readln()
+        if (password == "password") println("Доступ предоставлен!")
         else println("Вы ввели неверный пароль!")
     } else println("Нет такого пользователя!")
 }
