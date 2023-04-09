21703fbe6ab355f9a2ab43dcc394a0fad14332b3
TeterinMN <Scorpion.TMN@ya.ru>
2023-04-09 20:13:01 +0900

    KS-5-5. Задача 5 к Уроку 5. Изменил код

--- src/main/kotlin/lesson_5/Task_5.kt
+++ src/main/kotlin/lesson_5/Task_5.kt
@@ -1,17 +1,18 @@
 package lesson_5
 
-import java.util.concurrent.ThreadLocalRandom
+import kotlin.random.Random
 
 fun main() {
-    val numOne = ThreadLocalRandom.current().nextInt(1,100)
-    val numTwo = ThreadLocalRandom.current().nextInt(1,100)
+    val numOne = Random.nextInt(1, 101)
+    val numTwo = Random.nextInt(1, 101)
     println("Вам необходимо угадать два числа в диапазоне от 1 до 100")
     print("Введите первое число: ")
     val numberOne = readln().toInt()
     print("Введите второе число: ")
     val numberTwo = readln().toInt()
 
-    if (numberOne == numOne && numberTwo == numTwo) println("Вы выиграли главный приз!")
+    if (numberOne == numOne && numberTwo == numTwo || numberTwo == numOne && numberOne == numTwo)
+        println("Вы выиграли главный приз!")
     else if (numberOne == numOne || numberTwo == numTwo) println("Вы выиграли утешительный приз!")
     else println("Неудача! Крутите барабан!")
 
