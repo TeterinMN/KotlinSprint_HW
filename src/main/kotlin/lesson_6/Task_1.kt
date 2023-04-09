ccd69f83b8895982b213710d148a603102e80d01
TeterinMN <Scorpion.TMN@ya.ru>
2023-04-09 21:15:52 +0900

    KS-6-1. Задача 1 к Уроку 6. Переписал код согласно ТЗ

--- src/main/kotlin/lesson_6/Task_1.kt
+++ src/main/kotlin/lesson_6/Task_1.kt
@@ -1,65 +1,22 @@
 package lesson_6
 
 fun main() {
-    var appRunning = true
-    var login = ""
-    var password = ""
-
-    while (appRunning) {
+    print("Для регистрации введите логин: >> ")
+    val userLogin = readln()
+    print("Придумайте пароль: >> ")
+    val userPwd = readln()
+    while (true) {
         println()
-        print(
-            """
-            Зарегистрироваться  - 1
-            Войти в систему     - 2
-            Выйти из приложения - 3
-            Выберите действие:  >> 
-        """.trimIndent()
-        )
-
-        when (readln().toInt()) {
-            1 -> {
-                while (true) {
-                    println()
-                    print("Введите логин: ")
-                    login = readln()
-                    break
-                }
-                while (true) {
-                    print("Введите пароль: ")
-                    password = readln()
-                    break
-                }
-                println("Регистрация прошла успешно")
-            }
-
-            2 -> {
-                while (true) {
-                    println()
-                    print("Введите логин: ")
-                    val enteredLogin = readln()
-                    print("Введите пароль: ")
-                    val enteredPassword = readln()
-                    if (enteredLogin == login && enteredPassword == password) {
-                        println()
-                        println("Авторизация прошла успешно")
-                        break
-                    } else {
-                        println()
-                        println("Неверный логин или пароль")
-                    }
-                }
-            }
-
-            3 -> {
-                println()
-                println("Выход из приложения")
-                appRunning = false
-            }
-
-            else -> {
-                println()
-                println("Ошибка. Выберите пункт от 1 до 3")
-            }
+        print("Для входа введите логин: ")
+        val enteredLogin = readln()
+        print("Введите пароль: ")
+        val enteredPassword = readln()
+        if (enteredLogin == userLogin && enteredPassword == userPwd) {
+            println()
+            println("Авторизация прошла успешно")
+            break
+        } else {
+            println("Неверный логин или пароль повторите попытку")
         }
     }
 }
