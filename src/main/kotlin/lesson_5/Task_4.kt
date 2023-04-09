4251cb040ff6ca2dd0e6df89ebcb11193e469b53
TeterinMN <Scorpion.TMN@ya.ru>
2023-04-09 19:54:04 +0900

    KS-5-4. Задача 4 к Уроку 5. Все вернул, убрал лишние переменные

--- src/main/kotlin/lesson_5/Task_4.kt
+++ src/main/kotlin/lesson_5/Task_4.kt
@@ -1,12 +1,12 @@
 package lesson_5
 
+const val LOGIN = "batman"
+const val PWD = "password"
 fun main() {
     print("Добро пожаловать в приложение. \nВведите логин: ")
-    val login = readln()
-    if (login == "batman") {
+    if (readln() == LOGIN) {
         print("Введите пароль: ")
-        val password = readln()
-        if (password == "password") println("Доступ предоставлен!")
+        if (readln() == PWD) println("Доступ предоставлен!")
         else println("Вы ввели неверный пароль!")
     } else println("Нет такого пользователя!")
 }
