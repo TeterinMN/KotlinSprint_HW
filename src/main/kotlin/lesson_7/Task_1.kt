6a032ec4309ff5270e3c6b5a3c9413979a68e3f4
TeterinMN <Scorpion.TMN@ya.ru>
2023-04-12 15:47:30 +0900

    KS-7-1. Задача 1 к Уроку 7. Код без использования цикла (((

--- src/main/kotlin/lesson_7/Task_1.kt
+++ src/main/kotlin/lesson_7/Task_1.kt
@@ -1,11 +1,19 @@
 package lesson_7
 
 fun main() {
-    val number: IntRange = 1..10
-    val symbol: CharRange = 'a'..'z'
-    
-    for (i in 1..3) {
-        print(number.random())
-        print(symbol.random())
-    }
+    val number1: IntRange = 1..10
+    val symbol1: CharRange = 'a'..'z'
+    val number2: IntRange = 1..10
+    val symbol2: CharRange = 'a'..'z'
+    val number3: IntRange = 1..10
+    val symbol3: CharRange = 'a'..'z'
+
+    val n1 = number1.random()
+    val s1 = symbol1.random()
+    val n2 = number2.random()
+    val s2 = symbol2.random()
+    val n3 = number3.random()
+    val s3 = symbol3.random()
+
+    print("$n1$s1$n2$s2$n3$s3")
 }
