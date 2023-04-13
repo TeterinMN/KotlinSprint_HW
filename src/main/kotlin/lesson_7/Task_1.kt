ca6d4513ee35e5130883e2727fa4239f7ca60ccc
TeterinMN <ScorpionTMN@ya.ru>
2023-04-13 10:22:08 +0900

    KS-7-1. Задача 1 к Уроку 7 - изменил диапазон

--- src/main/kotlin/lesson_7/Task_1.kt
+++ src/main/kotlin/lesson_7/Task_1.kt
@@ -1,11 +1,11 @@
 package lesson_7
 
 fun main() {
-    val number1: IntRange = 1..10
+    val number1: IntRange = 1 until 10
     val symbol1: CharRange = 'a'..'z'
-    val number2: IntRange = 1..10
+    val number2: IntRange = 1 until  10
     val symbol2: CharRange = 'a'..'z'
-    val number3: IntRange = 1..10
+    val number3: IntRange = 1 until  10
     val symbol3: CharRange = 'a'..'z'
 
     print("${number1.random()}${symbol1.random()}" +
