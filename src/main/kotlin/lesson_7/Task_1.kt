12dc88097c3dafc1a287fab19b583a826f3e70bf
TeterinMN <ScorpionTMN@ya.ru>
2023-04-14 09:14:54 +0900

    KS-7-1. Задача 1 к Уроку 7. Устранил замечания

--- src/main/kotlin/lesson_7/Task_1.kt
+++ src/main/kotlin/lesson_7/Task_1.kt
@@ -1,14 +1,16 @@
 package lesson_7
 
 fun main() {
-    val number1: IntRange = 1 until 10
+    val number1: IntRange = 0 until 10
     val symbol1: CharRange = 'a'..'z'
-    val number2: IntRange = 1 until  10
+    val number2: IntRange = 0 until 10
     val symbol2: CharRange = 'a'..'z'
-    val number3: IntRange = 1 until  10
+    val number3: IntRange = 0 until 10
     val symbol3: CharRange = 'a'..'z'
 
-    print("${number1.random()}${symbol1.random()}" +
-            "${number2.random()}${symbol2.random()}" +
-            "${number3.random()}${symbol3.random()}")
+    print(
+        "${number1.random()}${symbol1.random()}" +
+                "${number2.random()}${symbol2.random()}" +
+                "${number3.random()}${symbol3.random()}"
+    )
 }
