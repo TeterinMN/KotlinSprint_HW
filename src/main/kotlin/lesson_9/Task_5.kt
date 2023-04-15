0206ec5a74e6b7c5834fa7f22cdaf2a0a0206784
TeterinMN <Scorpion.TMN@ya.ru>
2023-04-15 21:56:51 +0900

    KS-9-4. Задача 4 к Уроку 9

---
+++ src/main/kotlin/lesson_9/Task_5.kt
@@ -0,0 +1,26 @@
+package lesson_9
+/*
+Задача 5 к Уроку 9
+Напиши программу, которая запрашивает у пользователя пять названий ингредиентов (каждый по отдельности).
+ – результат должен исключать повторы;
+ – список должен выводиться в алфавитном порядке;
+ – все элементы списка выведены в одну строку, разделяя слова запятыми;
+ – первое слово должно быть напечатано с заглавной буквы, в конце списка должна стоять точка.
+ */
+
+fun main() {
+    val listIngredients = mutableSetOf<String>()
+
+    while (listIngredients.size < 5) {
+        print("Введите название ингредиента: ")
+        val ingredient = readln()
+        if (ingredient.isNotEmpty()) {
+            listIngredients.add(ingredient)
+        }
+    }
+    val listSorted = listIngredients.sorted().map { it.replaceFirstChar { it.uppercase() } }
+    listSorted.forEach {
+        print("$it ")
+    }
+    print(".")
+}
