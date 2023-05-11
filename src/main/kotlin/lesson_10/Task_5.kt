40e5c93b4c9bedaab9bc68aa08f36f093850788d
TeterinMN <Scorpion.TMN@ya.ru>
2023-05-11 16:46:50 +0900

    Переписал задание

--- src/main/kotlin/lesson_10/Task_5.kt
+++ src/main/kotlin/lesson_10/Task_5.kt
@@ -19,74 +19,59 @@ package lesson_10
 Действия в программе должны быть распределены по соответствующим методам без дублирования кода.
  */
 
-const val LOGIN_MIN_LENGTH = 4
-const val PWD_MIN_LENGTH = 4
-fun main() {
-    var login: String
-    println("Для регистрации придумайте логин и сгенерируйте пароль")
-    while (true) {
-        print(" Логин: >> ")
-        login = readln()
-        if (checkingLengthLogin(login)) println("Слишком короткий логин, логин должен быть не менее 4 символов")
-        else break
-    }
-    val password = pwdGenerator()
-    authorization(login, password)
+const val LENGTH_USER_NAME = 4
+const val PWD_MIN_LENGTH = 3
+var userLogin = ""
+var userPassword = ""
 
+fun main() {
+    userRegistration()
+    userAuthorization()
+    smsGenerator()
 }
 
-fun pwdGenerator(): String {
+fun userRegistration() {
+    do {
+        print("Придумайте логин >> ")
+        userLogin = readln()
+    } while (userLogin.length < LENGTH_USER_NAME)
     val number = 0 until 10
     val sym: CharRange = ' '..'@'
     val result = mutableListOf<String>()
     for (i in 1..PWD_MIN_LENGTH) {
         result.add(number.random().toString())
         result.add(sym.random().toString())
+        userPassword = result.joinToString("")
     }
-    print("Пароль: >> ")
-    result.forEach { print(it) }
-    return result.joinToString("")
-
+    println("Ваш сгенерированный пароль: $userPassword")
+    println()
 }
 
-fun checkingLengthLogin(login: String): Boolean {
-    return login.length < LOGIN_MIN_LENGTH
+fun userAuthorization() {
+    var login: String
+    var password: String
+    do {
+        print("АВТОРИЗАЦИЯ\nВведите логин: >> ")
+        login = readln()
+        print("Введите пароль: >> ")
+        password = readln()
+        if (userLogin != login || userPassword != password)
+            println("Неверно введен логин или пароль")
+    } while (userLogin != login || userPassword != password)
 }
 
-fun authorization(loginUser: String, passwordUser: String) {
+fun smsGenerator() {
     while (true) {
+        val result = (1000..9999).random().toString()
         println()
-        println("Авторизуйтесь")
-        print(" Логин: >> ")
-        val login = readln()
-        print("Пароль: >> ")
-        val password = readln()
-        if (login != loginUser || password != passwordUser)
-            println("Неверно введен логин или пароль!")
-        else {
-            while (true) {
-                val sms = smsGenerator()
-                println(sms)
-                print("Введите код подтверждения: >> ")
-                val smsUser = readln()
-                if (sms != smsUser) {
-                    println("Повторите ввод кода!")
-                } else {
-                    println("Вы успешно авторизовались!")
-                    break
-                }
-            }
+        println("Подтвердите вход кодом: $result")
+        print("Введите код подтверждения: >> ")
+        val smsUser = readln()
+        if (result != smsUser) {
+            println("Повторите ввод кода!")
+        } else {
+            println("Вы успешно авторизовались!")
+            break
         }
-        break
     }
-}
-
-fun smsGenerator(): String {
-    var result = ""
-    while (result.length < 4) {
-        val sms = (0..4).random()
-        val temp = sms.toString()
-        result += temp
-    }
-    return result
 }
