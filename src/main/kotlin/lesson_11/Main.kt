6a1e6f6a58b2bc71ae42a5a75535aaf919318486
TeterinMN <Scorpion.TMN@ya.ru>
2023-05-24 10:32:35 +0900

    KS-11-1. Задача 1 к Уроку 11

---
+++ src/main/kotlin/lesson_11/Main.kt
@@ -0,0 +1,24 @@
+package lesson_11
+
+fun  main() {
+    val userOne = User(
+        id = 1,
+        name = "Mikhail",
+        password = "Vip01@",
+        email = "Scorpio@r14-udc.ru"
+    )
+    val userTwo = User(
+        id = 2,
+        name = "Julia",
+        password = "Vip02@",
+        email = "Julia@r14-udc.ru"
+    )
+    println("ID Записи: ${userOne.id}")
+    println("Имя пользователя: ${userOne.name}")
+    println("E-Mail: ${userOne.email}")
+    println()
+    println("ID Записи: ${userTwo.id}")
+    println("Имя пользователя: ${userTwo.name}")
+    println("E-Mail: ${userTwo.email}")
+
+}
---
+++ src/main/kotlin/lesson_11/User.kt
@@ -0,0 +1,10 @@
+package lesson_11
+
+class User(
+    val id: Int,
+    val name: String,
+    val password: String,
+    val email: String,
+) {
+
+}
